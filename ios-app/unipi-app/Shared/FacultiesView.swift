f8368defb015798828ea86eac2614b84f0319e95
Giovanni Catania <giovanni.catania@ynap.com>
2021-10-28 14:10:43 +0200

    implement faculties view

--- ios-app/unipi-app/Shared/FacultiesView.swift
+++ ios-app/unipi-app/Shared/FacultiesView.swift
@@ -9,14 +9,40 @@ import SwiftUI
 import API
 
 struct FacultiesView: View {
+    let apiClient = APIClient(baseURL: "https://unipi-api.herokuapp.com")
+    let getPathsRequest = API.PathsOp.Request()
+        
+    @State var data: [(String, String)] = []
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .onAppear(perform: {  })
+        List(data, id: \.0) { element in
+            Cell(element: element.1)
+                .onTapGesture {
+                    print("tapped: \(element.1)")
+            }
+        }.onAppear(perform: { apiClient.makeRequest(getPathsRequest) { response in
+                
+                switch response.result {
+                    
+                case let .success(apiResponseValue):
+                    switch apiResponseValue {
+                    case let .status200(paths):
+                        data = paths.reduce([], { $0 + [($1.key, $1.value)] })
+                    default:
+                        print("unexpected status code")
+                    }
+                case let .failure(apiError):
+                    print("GetPaths failed with \(apiError)")
+                }
+            }
+            })
     }
 }
 
-struct FacultiesView_Previews: PreviewProvider {
-    static var previews: some View {
-        FacultiesView()
+struct Cell: View {
+    let element: String
+    
+    var body: some View {
+        Text(element)
     }
 }
