86b949a101f87f374e998675a364d6a51f9b4f91
nicola baldi (@naighes) <nic.baldi@gmail.com>
2021-10-28 14:32:31 +0200

    cleaning up persistence stuff and default ContentView.

--- ios-app/unipi-app/Shared/FacultiesView.swift
+++ ios-app/unipi-app/Shared/FacultiesView.swift
@@ -1,16 +1,9 @@
-//
-//  FacultiesView.swift
-//  unipi-app
-//
-//  Created by Giovanni Catania on 28/10/21.
-//
-
 import SwiftUI
 import API
 
 struct FacultiesView: View {
     let apiClient = APIClient(baseURL: "https://unipi-api.herokuapp.com")
-    let getPathsRequest = API.PathsOp.Request()
+    let pathsOpRequest = API.PathsOp.Request()
         
     @State var data: [(String, String)] = []
     
@@ -18,24 +11,28 @@ struct FacultiesView: View {
         List(data, id: \.0) { element in
             Cell(element: element.1)
                 .onTapGesture {
-                    print("tapped: \(element.1)")
-            }
-        }.onAppear(perform: { apiClient.makeRequest(getPathsRequest) { response in
-                
-                switch response.result {
-                    
-                case let .success(apiResponseValue):
-                    switch apiResponseValue {
-                    case let .status200(paths):
-                        data = paths.reduce([], { $0 + [($1.key, $1.value)] })
-                    default:
-                        print("unexpected status code")
-                    }
-                case let .failure(apiError):
-                    print("GetPaths failed with \(apiError)")
+                    let defaults = UserDefaults.standard
+                    defaults.set(element.1, forKey: "facultyId")
+                    defaults.set(element.0, forKey: "facultyName")
+
+                    // TODO: ensure selection is not missing
+                    // TODO: feedback to user
+                    // TODO: go to the next view (type user credentials)
                 }
+        }.onAppear(perform: { apiClient.makeRequest(pathsOpRequest) {
+            response in
+            switch response.result {        
+            case let .success(apiResponseValue):
+                switch apiResponseValue {
+                case let .status200(paths):
+                    data = paths.reduce([], { $0 + [($1.key, $1.value)] })
+                default:
+                    print("unexpected status code")
+                }
+            case let .failure(apiError):
+                print("GetPaths failed with \(apiError)")
             }
-            })
+        }})
     }
 }
 
--- ios-app/unipi-app/Shared/unipi_appApp.swift
+++ ios-app/unipi-app/Shared/unipi_appApp.swift
@@ -1,18 +1,19 @@
-//
-//  unipi_appApp.swift
-//  Shared
-//
-//  Created by Giovanni Catania on 28/10/21.
-//
-
 import SwiftUI
 
 @main
 struct unipi_appApp: App {
-    let persistenceController = PersistenceController.shared
-
     var body: some Scene {
         WindowGroup {
+            /* TODO
+            // FacultiesView will be shown just for the initialization phase.
+            // once the choice will be set within UserDefaults, we can skip FacultiesView at all.
+
+            if let facultyId = UserDefaults.standard.data(forKey: "facultyId") {
+                FacultiesView()
+            } else { // image is not present. set a default image
+                CredentialsView()
+            }
+            */
             FacultiesView()
         }
     }
